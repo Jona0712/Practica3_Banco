4e2186a0b55f62e18b8762f07130f2de5501405f
Jonnathan Castillo <jonnantonicas@gmail.com>
2019-03-27 10:44:57 -0600

    Proyecto

---
+++ Practica3_Banco/Practica3_Banco/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Practica3_Banco.Startup))]
+namespace Practica3_Banco
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
