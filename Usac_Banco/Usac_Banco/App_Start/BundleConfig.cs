61201dd0f11d2fe3ea4c2cc18e2a653cf5c23ee6
Rafael Antonio Morales <rafael.7.morales@gmail.com>
2019-04-02 20:11:44 -0600

    registro e inicio de sesion

--- Usac_Banco/Usac_Banco/App_Start/BundleConfig.cs
+++ Usac_Banco/Usac_Banco/App_Start/BundleConfig.cs
@@ -25,7 +25,7 @@ namespace Usac_Banco
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/sb-admin-2.min.css"));
         }
     }
 }
--- Usac_Banco/Usac_Banco/Controllers/usuariosController.cs
+++ Usac_Banco/Usac_Banco/Controllers/usuariosController.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
+﻿using System.Data.Entity;
 using System.Linq;
 using System.Net;
-using System.Web;
 using System.Web.Mvc;
-using Usac_Banco;
+using Usac_Banco.Models;
 
 namespace Usac_Banco.Controllers
 {
@@ -17,7 +13,16 @@ namespace Usac_Banco.Controllers
         // GET: usuarios
         public ActionResult Index()
         {
-            return View(db.usuario.ToList());
+            //return View(db.usuario.ToList());
+            if (Session["codigo"] != null)
+            {
+                usuario usu = db.usuario.Find(Session["codigo"]);
+                ViewBag.codigo = usu.codigo.ToString();
+                ViewBag.nombre = usu.nombre + " " + usu.apellido;
+                //ViewBag.cuenta = numero de cuenta
+                return View();
+            }
+            return RedirectToAction("Login");
         }
 
         // GET: usuarios/Details/5
@@ -35,6 +40,42 @@ namespace Usac_Banco.Controllers
             return View(usuario);
         }
 
+
+        public ActionResult Login()
+        {
+            if(Session["codigo"] != null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(Login model)
+        {
+            if (ModelState.IsValid)
+            {
+                usuario usuario = db.usuario.Find(model.codigo);
+                if (usuario != null && model.usua == usuario.usua && model.pass == usuario.pass)
+                {
+                    Session["codigo"] = usuario.codigo;
+                    //Session["usuario"] = usuario.nombre +" "+usuario.apellido;
+                    //Session["usuario"] = usuario.usua; numero de cuenta
+                    return RedirectToAction("Index");
+                }
+                return View();
+            }
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            Session["codigo"] = null;
+            return RedirectToAction("Login");
+        }
+
         // GET: usuarios/Create
         public ActionResult Create()
         {
@@ -46,7 +87,7 @@ namespace Usac_Banco.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "codigo,nombre,apellido,usua,correo,pass")] usuario usuario)
+        public ActionResult Create([Bind(Include = "nombre,apellido,usua,correo,pass")] usuario usuario)
         {
             if (ModelState.IsValid)
             {
